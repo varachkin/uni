4a3de23024dc07546d78e026c59ff0006b31695b
Aliaksandr Varachkin <a.varachkin@vendorobotics.com>
2025-01-02 11:30:11 +0100

    feat

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './assets/css/index.css'
 import App from './App.tsx'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App children={undefined} />
