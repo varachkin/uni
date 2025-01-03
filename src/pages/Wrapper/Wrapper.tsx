bf3ab380a0daf24cd159988fae2eaf72af39eb9c
Aliaksandr Varachkin <a.varachkin@vendorobotics.com>
2025-01-03 14:00:44 +0100

    fix

--- src/pages/Wrapper/Wrapper.tsx
+++ src/pages/Wrapper/Wrapper.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router";
 import { useSelector } from "react-redux";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 import { RootState } from "../../store/store";
 import SecretDevelopmentMode from "../../components/SecretDevelopmentMode/SecretDevelopmentMode";
 import Header from "../../components/Header/Header";
@@ -15,42 +15,7 @@ interface WrapperProps {
 export function Wrapper({ children }: WrapperProps): JSX.Element {
     const { devMode } = useSelector((state: RootState) => state.configurationReducer)
     const { pathname } = useLocation();
-    const enterFullscreen = () => {
-        const element = document.documentElement; // Fullscreen for the entire page
-        if (element) {
-            if (element.requestFullscreen) {
-                element.requestFullscreen();
-            } else if (element.requestFullscreen) {
-                element.requestFullscreen();
-            } else if (element.requestFullscreen) {
-                element.requestFullscreen();
-            } else if (element.requestFullscreen) {
-                element.requestFullscreen();
-            }
-        }
-    };
 
-    useEffect(() => {
-        enterFullscreen()
-        const preventNavigation = () => {
-            history.pushState(null, '', window.location.href);
-        };
-
-        const handlePopState = () => {
-            const el = document.documentElement;
-            if (el.requestFullscreen) {
-                el.requestFullscreen();
-            }
-        };
-
-        window.addEventListener('popstate', handlePopState);
-        window.addEventListener('popstate', preventNavigation);
-
-        return () => {
-            window.removeEventListener('popstate', handlePopState);
-            window.removeEventListener('popstate', preventNavigation);
-        };
-    }, [pathname])
     return (
         <>
             {devMode && <DevPanel />}
