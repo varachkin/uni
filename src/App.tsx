d68fb1347d186be982e2a3b7f4a3e88fab82fd13
Aliaksandr Varachkin <a.varachkin@vendorobotics.com>
2025-01-08 08:54:33 +0100

    pwa

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import AppRouter from "./AppRouter.tsx";
 import { Provider } from "react-redux";
 import { setupStore } from "./store/store";
 import { SnackbarProvider } from 'notistack';
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 
 interface IAppProps {
   children: ReactNode;
 }
 
 export default function App(props : IAppProps) {
+  useEffect(() => {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Safari
+      document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+      document.documentElement.msRequestFullscreen();
+    }
+  }, []);
+  
+  
   const store = setupStore()
   return (
     <>
